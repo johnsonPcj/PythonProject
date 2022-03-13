1c8015d72e886753009d4e3f69c0b9819da025f2
peilins.liu <liuplin0408@qq.com>
2022-03-13 12:51:18 -0700

    3 variables linear regression file

--- gradient_descent3.py
+++ gradient_descent3.py
@@ -1,7 +1,8 @@
 # USAGE
-# python 3 gradient_decent.py -w1 8 -w2 12 -b -1
+# python gradient_descent.py -w1 8 -w2 12 -w3 -20 -b -100
 
 import argparse
+import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -9,23 +10,31 @@ from matplotlib import pyplot as plt
 ap = argparse.ArgumentParser()
 ap.add_argument("-w1", type=float, default=8,
                 help="coefficient w1 initial ")
-ap.add_argument("-w2", type=float, default=12,
+ap.add_argument("-w2", type=float, default= 12,
                 help="coefficient w1 initial")
-ap.add_argument("-b", type=float, default=-1,
+ap.add_argument("-w3", type=float, default=-20,
+                help="coefficient w1 initial")
+ap.add_argument("-b", type=float, default=-100,
                 help="coefficient b initial")
 
 args = vars(ap.parse_args())
 w1 = args["w1"]  # 8,10,10264
 w2 = args["w2"]  # 12,15, 49350
+w3 = args["w3"]  # 12,15, 49350
 b = args["b"]  # -1,-10, -100533.75
 
-m = 11
-x1 = np.array([68, 95, 102, 130, 60, 45, 30, 80, 120, 113, 150])
-x2 = np.array([3, 2, 3, 1, 2, 3, 1, 3, 1, 1, 1])
-y = np.array([714.592, 956.877, 1053.582, 1293.667, 600.000, 520.000, 280.000, 845.000, 1150.000, 1120.000, 1490.234])
+df1 = pd.read_excel('house_value.xlsx')  # 读取xlsx中的第一个sheet
+#x1 = np.array([68, 95, 102, 130, 60, 45, 30, 80, 120, 113, 150])
+x1 = np.array(df1.iloc[:, 0].values) #area
+x2 = np.array(df1.iloc[:, 1].values/1000) # income
+x3 = np.array(df1.iloc[:, 2].values) # years
+#y = np.array([714.592, 956.877, 1153.582, 1293.667, 600.000, 520.000, 280.000, 845.000, 1150.000, 1120.000, 1490.234])
+y = np.array(df1.iloc[:, 3].values/1000) # value
 
-learn_rate1 = 0.0004
-learn_rate2 = 0.05
+m = x1.size
+
+learn_rate1 = 0.0001
+learn_rate2 = 0.0004
 iltnum = 0
 loss_value = []
 
@@ -35,18 +44,20 @@ plt.style.use("ggplot")
 
 # for i in range(100):
 while (True):
-    y_pred = w1 * x1 + w2 * x2 + b  # function,linear regression
+    y_pred = w1 * x1 + w2 * x2 + w3 * x3 +b  # function,linear regression
     loss = (y_pred - y) ** 2  # loss function ,is a list,store every y_pred(i)-y(i) loss value,i=[0:m]
     loss_value.append((0.5 * loss.sum() / m)) # statistics ,calculate all the(y_pred(i)-y(i)) loss values summary
     grad_w1 = 0.5 * np.sum((y_pred - y) * x1) / m  # partial calculus, calculate gradient for w1
     grad_w2 = 0.5 * np.sum((y_pred - y) * x2) / m  # partial calculus, calculate gradient for w2
+    grad_w3 = 0.5 * np.sum((y_pred - y) * x3) / m  # partial calculus, calculate gradient for w2
     grad_b = 0.5 * np.sum(y_pred - y) / m          # partial calculus, calculate gradient for b
     w1 -= learn_rate1 * grad_w1     # gradient decent for w1 , from gradient decent direction to  get the next w1
     w2 -= learn_rate2 * grad_w2     # gradient decent for w2 , from gradient decent direction to  get the next w2
+    w3 -= learn_rate2 * grad_w3
     b -= learn_rate2 * grad_b       # gradient decent for b , from gradient decent direction to  get the next b
 
-    print("%-7d:loss %-6.3f, grad_w1:%-10.4f,grad_w2:%-10.4f,grad_b:%-10.5f ,(w1,w2,b):%-7.2f %-7.2f %-7.2f" \
-          % (iltnum, loss_value[iltnum], grad_w1, grad_w2, grad_b, w1, w2, b))
+    print("%-7d:loss %-6.3f, grad_w1:%-8.4f,grad_w2:%-8.4f,grad_w3:%-8.4f,grad_b:%-8.5f ,(w1,w2,w3,b):%-5.2f %-5.2f %-5.2f %-5.2f" \
+          % (iltnum, loss_value[iltnum], grad_w1, grad_w2, grad_w3,grad_b, w1, w2, w3,b))
     if iltnum == 0:
         ax[0, 0].set_title("Predict vs True value:{} iteration".format(iltnum))
         ax[0, 0].set_xlabel("Area #")
@@ -68,7 +79,7 @@ while (True):
                 or (round(abs(grad_w1), 3) <= 0.001 and round(abs(grad_b), 2) <= 0.01):
             break
     iltnum += 1
-print(w1, w2, b)
+print(w1, w2, w3, b)
 
 ax[1, 0].set_title("Predict vs True value:{} iteration".format(iltnum))
 ax[1, 0].set_xlabel("Area #")
@@ -84,9 +95,9 @@ x = np.array(x)
 ax[1, 1].set_title("loss value curve:{} iteration".format(iltnum))
 ax[1, 1].set_xlabel("iltnum #")
 ax[1, 1].set_ylabel("loss Value")
-function = "f(x1,x2)={} *x1 + {} *x2 + {}".format(round(w1, 3), round(w2, 3), round(b, 3))
+function = "f(x1,x2, x3)={} *x1 + {} *x2 + {} *x3 + {}".format(round(w1, 3), round(w2, 3), round(w3, 3), round(b, 3))
 ax[1, 1].plot(x, loss_value, color='purple',
-              label=function + f"\nInitial Predict Coefficient<w1o,w2o,b0>:({args['w1']},{args['w2']},{args['b']})" \
+              label=function + f"\nInitial Predict Coefficient<w1o,w2o,w3o,b0>:({args['w1']},{args['w2']},{args['w3']},{args['b']})" \
                     + f"\n Loss value Maximum = {round(loss_value[0], 1)},Minimum = {round(loss_value[iltnum], 1)}")
 # plt.ylim(331.805, 331.826)
 ax[1, 1].legend()
